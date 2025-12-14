a802448255e68d6254b773f2930303e32e01e999
mdrieadmia <rieadyt@gmail.com>
2025-12-14 22:12:36 +0600

    Basic Type Practice

---
+++ module1/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const myName = "MD RIEAD MIA";
+const myRoll = 2241091255;
+const isEnrolled = true;
+const details = null;
+const mySub = ["Bangla", "English", "Math"];
+const mySubCode = [101, 103, 107];
--- module1/src/index.ts
+++ module1/src/index.ts
@@ -1,2 +1,11 @@
-const myName : string  = "Riead";
-console.log(name)
+const myName : string = "MD RIEAD MIA"
+const myRoll : number = 2241091255
+const isEnrolled : boolean = true
+const details : null = null
+
+const mySub : string[] = ["Bangla", "English", "Math"]
+const mySubCode : number[] = [101, 103, 107]
+const myFriendInfo : [string, number] = ["Rony", 2210091554]
+myFriendInfo.push('Dhaka')
+
+console.log(myFriendInfo)
