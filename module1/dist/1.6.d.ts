39f3e634e9d3d6edb85f1f35d593b99fc72050e2
mdrieadmia <rieadyt@gmail.com>
2025-12-18 15:48:43 +0600

    Function Type Practice

---
+++ module1/dist/1.6.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=1.6.d.ts.map
---
+++ module1/src/1.6.ts
@@ -0,0 +1,23 @@
+function add(num1 : number, num2:number):number {
+    return num1+num2;
+}
+
+add(2, 3)
+
+
+const addNumbers = (num1:number, num2:number = 10):number => num1+num2
+
+const userData = {
+    name: "Riead",
+    balance: 10,
+    addBalance(balance:number){
+        return `My Balance is now: ${this.balance + balance}`
+    }
+}
+
+
+const arr:number[] = [1, 2, 5, 7]
+
+const newArr: number[]  = arr.map((num: number): number=> num*num)
+
+console.log(newArr)
