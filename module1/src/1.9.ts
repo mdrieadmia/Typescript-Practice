5865cd593497ce60c8f26db4e713a6e9a2f5fc42
mdrieadmia <rieadyt@gmail.com>
2025-12-21 18:46:03 +0600

    Type Alias

---
+++ module1/src/1.9.ts
@@ -0,0 +1,26 @@
+type Student = {
+    name: string,
+    age: number,
+    gender: string,
+    contact: string,
+}
+
+
+const student1 : Student = {
+    name: 'Riead',
+    age: 24,
+    gender: 'Male',
+    contact: '01863931220',
+}
+
+const student2 : {
+    name: string,
+    age: number,
+    gender: string,
+    contact: string,
+} = {
+    name: 'Riead',
+    age: 24,
+    gender: 'Male',
+    contact: '01863931220',
+}
