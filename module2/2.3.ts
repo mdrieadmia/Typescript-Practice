c16f3502613fc1d7220e20569e7991a08bfbdd2a
mdrieadmia <rieadyt@gmail.com>
2025-12-25 22:47:16 +0600

    Generic Array Practice

---
+++ module2/2.3.ts
@@ -0,0 +1,12 @@
+{
+
+    //Generic Type
+
+    type GenericArray<T> = Array<T>
+
+    // const rollNumbers : number[]  = [3, 5, 8]
+    const rollNumbers : GenericArray<number>  = [3, 5, 8]
+
+    // const mentors : string[] = ["Mir", "Mezba"]
+    const mentors : GenericArray<string> = ["Mir", "Mezba"]
+}
